1ae85c8ce4d8efd7d4381932826147b7cdaad6fc
kandadave <kandachumo@gmail.com>
2025-08-21 09:32:07 +0300

    relationships

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,34 @@
-class Pet:
-    pass
-
 class Owner:
-    pass
+    def __init__(self, name):
+        self.name = name
+        self._pets = []
+
+    def pets(self):
+        return self._pets
+
+    def add_pet(self, pet):
+        if not isinstance(pet, Pet):
+            raise Exception("Argument must be an instance of Pet")
+        if pet not in self._pets:
+            pet.owner = self
+            self._pets.append(pet)
+
+    def get_sorted_pets(self):
+        return sorted(self._pets, key=lambda pet: pet.name)
+
+
+class Pet:
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+    all = []
+
+    def __init__(self, name, pet_type, owner=None):
+        if pet_type not in self.PET_TYPES:
+            raise Exception(f"pet_type must be one of {self.PET_TYPES}")
+        if owner is not None and not isinstance(owner, Owner):
+            raise Exception("owner must be an instance of Owner")
+        self.name = name
+        self.pet_type = pet_type
+        self.owner = owner
+        if owner is not None:
+            owner.add_pet(self)
+        Pet.all.append(self)
